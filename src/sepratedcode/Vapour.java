f9db2a2063f0b4b1662b78556a3dabc162b83d14
ARU0007 <aruneshagrahari95@gmail.com>
2024-02-15 10:16:25 +0530

    create a subclass Vapour.java and extend Water

---
+++ src/sepratedcode/Vapour.java
@@ -0,0 +1,10 @@
+package sepratedcode;
+
+//nother subclass vapour represent gaseous state of Water-
+
+public class Vapour extends Water{
+    @Override
+    public void state() {
+        System.out.println("Water is in gaseous state(Vapour)");
+    }
+}
