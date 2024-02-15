1d6b92b6b6fd85a11235c2e3c7ee6d1a8ce256d0
ARU0007 <aruneshagrahari95@gmail.com>
2024-02-15 10:16:05 +0530

    create a subclass Ice.java and extend Water

---
+++ src/sepratedcode/Ice.java
@@ -0,0 +1,10 @@
+package sepratedcode;
+
+//subclass Ice represents solid state of water-
+
+public class Ice extends Water{
+    @Override
+    public void state() {
+        System.out.println("Water is in solid state(Ice)");
+    }
+}
